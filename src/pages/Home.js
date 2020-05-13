2fd8843230933281772b87daf4f259f1f40f03cd
Chris Behrens <chrisbehrens@yahoo.com>
2020-05-12 20:52:19 -0700

    changed Chris image size on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ return(
     <img className="background" src={MenuImg}></img>
     <h2 className = 'title'> About Me</h2>
     <img className="chris" src={Chris}></img>
-    <h5> Hi, I'm Chris!</h5>
+    <h5 className="intro"> Hi, I'm Chris!</h5>
     <h5>I am a Full Stack Web Developer who is a critical thinker, team player and loves a challenge. Recently received a Full Stack Development certificate from The University of Arizona. I have built this portfolio to help showcases my skills with front end programs such as HTML5, CSS, JavaScript, Bootstrap, ReactJs and back end programs and frameworks such as Node.js, express, MongoDB and MySQL.</h5>
     <h5 className ="about">My experience as a Systems Engineer has given me knowledge of the cutting edge hardware and software being used to accelerate growth and new technologies in this space. I also have a strong business background that includes a Bachelor’s degree in Finance that can be used to put a monetary perspective on projects as well. I am excited to leverage my expertise, education and knowledge and become an asset to a software development team.</h5> 
     <h3 className='skillsHeader'>Current Technologies and Skill Set</h3>
