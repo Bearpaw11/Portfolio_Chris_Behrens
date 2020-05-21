bf3b137f80fcfd0ef518d67ef2ca7cd6574aca30
Chris Behrens <chrisbehrens@yahoo.com>
2020-05-20 21:37:46 -0700

    made grammer changes to porfolio cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -29,7 +29,7 @@ class App extends Component {
            <Card.Img className="projectImg" variant="top" src="https://images.unsplash.com/photo-1558541966-d1071f7329bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60.com/photo-1508697014387-db70aad34f4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
            <Card.Body>
              <Card.Title className="appTitle">Speech Therapy</Card.Title>
-             <Card.Text>This app allows you to practice your speech using voice-to-text technology. You will receive analysis on filler words you might be saying and the length of your speech. </Card.Text>
+             <Card.Text>This app allows you to practice your speech using voice-to-text technology. You will receive analysis on filler words you may be saying and the length of your speech. </Card.Text>
              <p> Technologies used: React, MySQL/Sequelize, Nodemailer, Express, Node, Bootstrap, HTML, CSS, Sass, WebSpeech API, Passport, Heroku.</p>
              <a href="https://github.com/Bearpaw11/speech_app/" target="_blank">Link to GitHub Repo</a><br></br>
              <a href="https://speechtherapyapp.herokuapp.com/" target='-blank'>Link to App</a>
@@ -41,7 +41,7 @@ class App extends Component {
            <Card.Img className="projectImg" variant="top" src="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
            <Card.Body>
              <Card.Title className="appTitle">Bottoms Up</Card.Title>
-             <Card.Text>This is Full Stack social media app aimed at those who have an interest in consuming adult libations, whether at home or out and about, painting the town red! </Card.Text>
+             <Card.Text>This is a Full Stack social media app aimed at those who have an interest in consuming adult libations, whether at home or out and about, painting the town red! </Card.Text>
              <p>Technologies used: Node.js, express, bcrypt, Passport, MySQL, sequelize, CSS, Bootstrap, Heroku</p>
              <a href="https://github.com/zace118/BottomsUp" target="_blank">Link to GitHub Repo</a><br></br>
              <a href="https://bottoms-up-app.herokuapp.com/" target='-blank'>Link to App</a>
@@ -53,7 +53,7 @@ class App extends Component {
            <Card.Img className="projectImg" variant="top" src="https://images.unsplash.com/photo-1508697014387-db70aad34f4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
            <Card.Body>
              <Card.Title className="appTitle">Weather Dashboard</Card.Title>
-             <Card.Text>Enter a city in the search box and the current weather pops up and also a 5 day forecast. The five most recent cities are saved in local stoarge and shown on the page.</Card.Text>
+             <Card.Text>Enter a city in the search box and the current weather pops up including a 5 day forecast. The five most recent cities are saved in local stoarge and shown on the page.</Card.Text>
              <p>Technologies used: HTML, CSS, JavaScript, jQuery, OpenWeatherMap API and moment.js</p>
              <br></br>
              <a href="https://github.com/Bearpaw11/Unit_06_Weather_Dashboard" target="_blank">Link to GitHub Repo</a><br></br>
@@ -66,7 +66,7 @@ class App extends Component {
            <Card.Img className="projectImg" variant="top" src="https://images.unsplash.com/photo-1552068751-34cb5cf055b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60.com/photo-1508697014387-db70aad34f4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
            <Card.Body>
              <Card.Title className="appTitle">ChatApp</Card.Title>
-             <Card.Text>Join a chatroom and live chat with other people. You can choose from multiple chat rooms to join. The Chat admin will notify you when others have joined and left the room</Card.Text>
+             <Card.Text>Join a chat room and live chat with other people. You can choose from multiple chat rooms to join. The Chat admin will notify you when others have joined and left the room</Card.Text>
              <p>Technologies used: CSS, JavaScript, Node.js, express, socket.io, MomentJS, Heroku</p>
              <br></br>
              <a href="https://github.com/Bearpaw11/ChatApp/" target="_blank">Link to GitHub Repo</a><br></br>
